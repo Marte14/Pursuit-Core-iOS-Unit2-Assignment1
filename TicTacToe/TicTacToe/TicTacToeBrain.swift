9b505296327d260d17b9cf7a778aaf7ad418202c
marte14 <kattymarte14@gmail.com>
2018-11-08 17:37:05 -0500

    1st commit

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
+//handles logic functions to check win to check input etc whos turn it is and store your input
 import Foundation
 
 class TicTacToeBrain {
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+    override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+   print ("we are making tck tac toe")
   }
 
 
